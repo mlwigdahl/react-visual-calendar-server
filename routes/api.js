fd68322c113b50c927d50152d09727b155d97921
Matt Wigdahl <mlwigdahl@gmail.com>
2017-03-29 22:09:34 -0500

    start of adding Babel and a more complex route

--- routes/api.js
+++ routes/api.js
@@ -12,4 +12,15 @@ router.get('/user/:userId/calendar', function(req, res, next) {
   res.json({ userId: userId, calendar: { stuff: 'stuff' } });
 });
 
+// TODO params not working...
+router.get('/user/:userId/dateRange?:startDate&:endDate', function(req, res, next) {
+  let userId, startDate, endDate;
+  // { userId, startDate, endDate } = req.params; // TODO get rest-spread working through Babel
+  userId = req.params.userId;
+  startDate = req.params.startDate;
+  endDate = req.params.endDate;
+
+  res.json({ userId: userId, startDate: startDate, endDate: endDate });
+})
+
 module.exports = router;
